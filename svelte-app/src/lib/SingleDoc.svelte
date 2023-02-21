0a791059ade87dfe3c0716d5aa6461370f4c86bf
vincent <vincentcharlebois@gmail.com>
2023-02-21 14:36:41 -0500

    component to include all document interactions

---
+++ svelte-app/src/lib/SingleDoc.svelte
@@ -0,0 +1,25 @@
+<script>
+    import Gravatar from 'svelte-gravatar';
+
+    import Ephemerality from "./Ephemerality.svelte";
+    import GetAttachment from "./GetAttachment.svelte";
+    import DocDetails from "./DocDetails.svelte";
+
+    let showDetails = false;
+
+    export let doc;
+    
+</script>
+
+<button on:click={() => (showDetails = !showDetails)} on:keypress={() => (showDetails = !showDetails)}>
+    <Gravatar style="margin-bottom:-3px;" email={doc.text} size="18" default="retro"/>
+</button>
+{#if showDetails}
+
+    <GetAttachment {doc} />
+    
+    <Ephemerality {doc} on:update />
+
+    <DocDetails {doc} />
+
+{/if}
