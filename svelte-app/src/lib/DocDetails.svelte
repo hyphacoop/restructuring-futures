26c9f24df3e60635a13ed44d067a442832ffbc67
vincent <vincentcharlebois@gmail.com>
2023-02-21 14:41:54 -0500

    doc details includes; type, size and deleteAfter log

---
+++ svelte-app/src/lib/DocDetails.svelte
@@ -0,0 +1,17 @@
+<script>
+    export let doc;
+    let showDetails = false;
+</script>
+<button on:click={() => (showDetails = !showDetails)}>
+    📄
+</button>
+{#if showDetails}
+    <div>
+        <p>
+            Attachment type: {doc.path.split(".")[doc.path.split(".").length - 1]}
+            <br />
+            Attachment size: {doc.attachmentSize / 1000} kb
+        </p>
+        <p>{@html doc.text}</p>
+    </div>
+{/if}
