7f0d9106fdfbd6aa411b9706293e0926f675cc66
vincent <vincentcharlebois@gmail.com>
2023-02-27 10:14:03 -0500

    extra style + formatting

--- svelte-app/src/lib/SingleDoc.svelte
+++ svelte-app/src/lib/SingleDoc.svelte
@@ -1,5 +1,5 @@
 <script>
-    import Gravatar from 'svelte-gravatar';
+    import Gravatar from "svelte-gravatar";
 
     import Ephemerality from "./Ephemerality.svelte";
     import GetAttachment from "./GetAttachment.svelte";
@@ -12,21 +12,29 @@
     export let studio = false;
     export let attachment = true;
     export let title = undefined;
-    
+
     let extended = false;
     let timespan = undefined;
 
-    $: console.log('studio', studio);
+    $: console.log("studio", studio);
 
-    if (title !== undefined && title.includes('<br>')) {
-            timespan = title.split('<br>').pop();
-            title = title;
-            extended = true;
+    if (title !== undefined && title.includes("<br>")) {
+        timespan = title.split("<br>").pop();
+        title = title;
+        extended = true;
     }
 </script>
 
-<button on:click={() => (showDetails = !showDetails)} on:keypress={() => (showDetails = !showDetails)}>
-    <Gravatar style="margin-bottom:-3px;" email={doc.text} size="18" default="retro"/>
+<button
+    on:click={() => (showDetails = !showDetails)}
+    on:keypress={() => (showDetails = !showDetails)}
+>
+    <Gravatar
+        style="margin-bottom:-3px;"
+        email={doc.text}
+        size="18"
+        default="retro"
+    />
 </button>
 {#if title}
     <h3>{@html title}</h3>
@@ -35,17 +43,35 @@
     <h4>{@html timespan}</h4>
 {/if}
 {#if showDetails}
-
-    {#if attachment}
-        <GetAttachment {doc} />
-    {/if}
-
-    <DocDetails {doc} {attachment} />
-
-    <Reply {doc} />
-
-    {#if !studio}
-        <Ephemerality {doc} on:update />
-    {/if}
-
+    <div class="flex">
+        {#if attachment}
+            <div>
+                <GetAttachment {doc} />
+            </div>
+        {/if}
+        <div>
+            <DocDetails {doc} {attachment} />
+        </div>
+        {#if attachment}
+            <div>
+                <Reply {doc} />
+            </div>
+        {/if}
+        {#if !studio}
+            <div>
+                <Ephemerality {doc} on:update />
+            </div>
+        {/if}
+    </div>
 {/if}
+
+<style>
+    .flex {
+        display: flex;
+        flex-direction: column;
+        flex-wrap: wrap;
+        justify-content: space-between;
+        align-items: center;
+        align-content: center;
+    }
+</style>
