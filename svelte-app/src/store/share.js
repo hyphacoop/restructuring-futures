2a2e2a1336182788de902f026b4d9f6d580b5163
Vincent Charlebois <vincentcharlebois@gmail.com>
2023-03-08 07:02:48 -0500

    share details for the +commons

--- svelte-app/src/store/share.js
+++ svelte-app/src/store/share.js
@@ -1,8 +1,10 @@
 import { readable } from 'svelte/store'
 
 const shareKeypair = readable({
-  "shareAddress": "+filesharing.b5measwv2ezjdw7zfrsgqpxlln4yvs5cvcefompv4gypg2ueflr3a",
-  "secret": "bkhu7fomnhbgqnecjnwstunaued7kxlvvazit43u7afizdfpvo4kq"
+  
+  "shareAddress": "+commons.b46htlmsjtybgbuqzkvxjq32vme4ipivbvr22gzkt26am3amnqkjq",
+  "secret": "btaimfn54wjmaq6f7ddn2yacpybutmlxclgup32ap5w4nucqfgsha"
+​
 });
 
 export default shareKeypair
