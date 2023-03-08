d0c4850b14717d25cfbf92c0ffb8c6cdb7bbb15b
Vincent Charlebois <vincentcharlebois@gmail.com>
2023-03-08 14:55:22 -0500

    added cycles of decay

--- svelte-app/src/lib/GridView.svelte
+++ svelte-app/src/lib/GridView.svelte
@@ -1,7 +1,10 @@
 <script>
   import FileSharing from "./FileSharing.svelte";
   import { onMount } from "svelte";
+
   import cache from "../store/cache";
+  import { time } from "../store/time";
+
   import SingleDoc from "./SingleDoc.svelte";
 
   let grid = [4, 8];
@@ -10,6 +13,10 @@
   $: row = `repeat(${grid[0]}, minmax(25%, auto))`;
 
   let documents = [];
+  let oracle = true;
+  let hours = [ 10800000000, 7200000000, 3600000000, 0];
+  let convertedTime = $time.getTime() * 1000;
+
 
   export let inStudio;
 
@@ -43,26 +50,32 @@
 
 <div>
     <h2>The Commons</h2>
+    <FileSharing {oracle} {inStudio}/>
     <div class='flex'>
         {#if documents.length === 0}
             <p>No files yet</p>
         {:else}
+          {#each hours as hour (hour)}
           <div
           class="container"
           style="grid-template-rows: {row}; grid-template-columns: {col};"
         >
             {#each { length: grid[0] } as _, i (i)}
               {#each { length: grid[1] } as _, j (j)}
-
+                  
                 {#each documents as doc (doc.textHash)}
 
                     {#if doc.path.split('/')[2] == j && doc.path.split('/')[3] == i}           
 
                     <div id={doc.textHash}>
 
-                        <SingleDoc {doc} on:update={updateUI} {inStudio}/>
+                        {#if (convertedTime > (doc.deleteAfter - hour)) && (convertedTime > (doc.deleteAfter - (hour - 3600000000)))}
+                        <SingleDoc {doc} on:update={updateUI} {inStudio} disabled={true} />
+                        {:else}
+                        <SingleDoc {doc} on:update={updateUI} {inStudio} />
+                        {/if}
 
-                    </div>
+                      </div>
                     {:else}
                     <div></div>
                     {/if}
@@ -70,6 +83,7 @@
               {/each}
             {/each}
           </div>
+          {/each}
         {/if}
       </div>
   </div>
@@ -79,7 +93,7 @@
 <style>
       h2 {
         text-align: center;
-        font-family: "Fungal Grow 900 Thickness 1000";
+        font-family: "Fungal Grow 400 Thickness 1000";
     }
   .container {
     display: grid;
