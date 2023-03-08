5142633a58faef1810435995ea41f461027739ea
Vincent Charlebois <vincentcharlebois@gmail.com>
2023-03-08 07:11:15 -0500

    rm Unexpected character

--- svelte-app/src/store/share.js
+++ svelte-app/src/store/share.js
@@ -4,7 +4,6 @@ const shareKeypair = readable({
   
   "shareAddress": "+commons.b46htlmsjtybgbuqzkvxjq32vme4ipivbvr22gzkt26am3amnqkjq",
   "secret": "btaimfn54wjmaq6f7ddn2yacpybutmlxclgup32ap5w4nucqfgsha"
-​
 });
 
 export default shareKeypair
