c1e445bd4c88696d46f059ea2a60972e1dc0814c
vincent <vincentcharlebois@gmail.com>
2023-06-08 08:46:22 -0400

    new artifact view

---
+++ svelte-app/src/lib/ArtifactIcon.svelte
@@ -0,0 +1,48 @@
+<script>
+    import { createEventDispatcher } from "svelte";
+    const dispatch = createEventDispatcher();
+
+    let images = [
+        "icons/mountain-phase-0-02.png",
+        "icons/mountain-phase-1-02.png",
+        "icons/mountain-phase-2-02.png",
+        "icons/mountain-phase-3-02.png",
+        "icons/mountain-phase-4-02.png",
+        "icons/mountain-phase-5-02.png",
+        "icons/mountain-phase-6-02.png",
+        "icons/mountain-phase-7-02.png",
+        "icons/mountain-phase-8-02.png",
+    ];
+
+    export let doc;
+    export let disabled = false;
+
+    let deletionTime = doc.deleteAfter;
+    let phase =
+        Math.floor((deletionTime - Date.now()) / (2548800000000 / 9)) % 9;
+
+    function getClick(doc) {
+        console.log('clicked');
+        dispatch('click', doc);
+    }
+</script>
+
+{#if disabled}
+    <div>
+        <img src={images[phase]} alt="document icon" style="filter: blur(1px);" />
+    </div>
+{:else}
+    <div 
+    on:click={getClick}
+    on:keypress={getClick}
+    >
+        <img src={images[phase]} alt="document icon" />
+    </div>
+{/if}
+
+<style>
+    img {
+        max-width: 100px;
+        max-height: 100px;
+    }
+</style>
---
+++ svelte-app/src/lib/ArtifactView.svelte
@@ -0,0 +1,44 @@
+<script>
+    import { createEventDispatcher } from "svelte";
+    const dispatch = createEventDispatcher();
+
+    import GetAttachment from "./GetAttachment.svelte";
+    import Reply from "./Reply.svelte";
+
+    export let selectedDocument;
+    export let attachment = true;
+    export let reply = true;
+    export let inStudio = false;
+
+    function close() {
+        dispatch("close");
+    }
+</script>
+<h2 class="mt-12">Title placeholder</h2>
+
+<div class='flex flex-row justify-between w-full'>
+{#if attachment}
+    <div class='attachmentbox'>
+        <GetAttachment doc={selectedDocument} />
+    </div>
+{/if}
+{#if reply}
+<div>
+    <Reply doc={selectedDocument} {inStudio}/>
+</div>
+{/if}
+</div>
+
+<button class='top-right' on:click={close}>close</button>
+
+<style>
+    .top-right {
+        position: absolute;
+        top:10px;
+        right: 10px;
+    }
+    .attachmentbox {
+        width:70%;
+        height:100%;
+    }
+</style>
