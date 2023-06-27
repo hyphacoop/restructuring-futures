5fdd0db6034be351f4ee3b930e30de36ecad5951
vincent <vincentcharlebois@gmail.com>
2023-06-27 12:53:13 -0400

    create writable store for cache, subscribe to replica

--- svelte-app/src/store/cache.js
+++ svelte-app/src/store/cache.js
@@ -1,17 +1,24 @@
 import * as Earthstar from 'earthstar';
-import { readable } from 'svelte/store'
+import { writable } from 'svelte/store';
 import replica from "./replica";
 
-let replicaDetails;
+// Create a writable store for cache
+const cacheDetails = writable(null);
 
-replica.subscribe(value => {
-    replicaDetails = value;
-});
-
-const cache = new Earthstar.ReplicaCache(replicaDetails.replica)
-
-const cacheDetails = readable({
-    cache
+// Subscribe to replica
+replica.subscribe(replicaDetails => {
+    if (!replicaDetails || !replicaDetails.replica) {
+        console.error("Replica is undefined or does not contain a valid replica property");
+        return;
+    }
+    // Create a new ReplicaCache every time the replica changes
+    try {
+        const newCache = new Earthstar.ReplicaCache(replicaDetails.replica);
+        // Update the cacheDetails store
+        cacheDetails.set(newCache);
+    } catch (error) {
+        console.error("Failed to create new cache:", error);
+    }
 });
 
 export default cacheDetails;
