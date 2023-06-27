3b234f04e7b7863a015fbc4485f76b54c5dd2ea7
vincent <vincentcharlebois@gmail.com>
2023-06-27 12:53:36 -0400

    Subscribe to the shareKeypair store and update replicaDetails

--- svelte-app/src/store/replica.js
+++ svelte-app/src/store/replica.js
@@ -1,24 +1,26 @@
 import * as Earthstar from 'earthstar';
 import { ReplicaDriverWeb } from "earthstar/browser";
-import { readable } from 'svelte/store'
+import { writable } from 'svelte/store';
 import shareKeypair from "./share.js";
 
-let shareDetails;
+const replicaDetails = writable(null);
 
+// Subscribe to the shareKeypair store and update replicaDetails when it changes
 shareKeypair.subscribe(value => {
-    shareDetails = value;
+    console.log("shareKeypair store changed:", value);
+    const shareAddress = value.shareAddress;
+    const secret = value.secret;
+
+    // Use shareAddress and secret to create Replica
+    const replica = new Earthstar.Replica({
+        // @ts-ignore
+        driver: new ReplicaDriverWeb(shareAddress),
+        // @ts-ignore
+        shareSecret: secret,
+    });
+
+    // Update the replicaDetails store
+    replicaDetails.set({ replica });
 });
 
-const replica = new Earthstar.Replica({
-    // @ts-ignore
-    driver: new ReplicaDriverWeb(shareDetails.shareAddress),
-    // @ts-ignore
-    shareSecret: shareDetails.secret,
-});
-
-
-const replicaDetails = readable({
-    replica
-});
-
-export default replicaDetails
+export default replicaDetails;
