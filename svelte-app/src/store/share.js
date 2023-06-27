43592e8ae2694a897a7357e6abbe411bd043596a
vincent <vincentcharlebois@gmail.com>
2023-06-27 12:54:06 -0400

    share is writable

--- svelte-app/src/store/share.js
+++ svelte-app/src/store/share.js
@@ -1,10 +1,10 @@
-import { readable } from 'svelte/store';
+import { writable } from 'svelte/store';
 
-const shareKeypair = readable({
-
+const initialKeypair = {
     "shareAddress": "+commons.b7q4gt64yiefosdafnmhvtxz43akzk6gw54aesdahtf4kdgpbyeia",
     "secret": "bigzorlvyjy5fy5a3kglfzcyfzramsic3cihj4rqphmd6x5wo5jcq"
+};
 
-});
+const shareKeypair = writable(initialKeypair);
 
-export default shareKeypair
+export default shareKeypair;
