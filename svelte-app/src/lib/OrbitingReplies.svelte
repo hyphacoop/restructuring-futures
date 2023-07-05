2ab542f94c5cebf35f3be49957325411d4326511
vincent <vincentcharlebois@gmail.com>
2023-07-04 20:52:40 -0400

    dynamic diameters for orbiting reply circles indicators #85

--- svelte-app/src/lib/OrbitingReplies.svelte
+++ svelte-app/src/lib/OrbitingReplies.svelte
@@ -4,6 +4,9 @@
   export let doc = null;
   export let disabled = false;
 
+  // The default diameter of the largest orbiting circles
+  const orbitDiameter = 260;
+
   let replies = [];
 
   const fetchReplies = async () => {
@@ -20,7 +23,7 @@
         pathStartsWith: basePath,
       },
     });
-      // Exclude the first reply (the document itself)
+    // Exclude the first reply (the document itself)
     replies = tempReplies.slice(1);
     console.log("Replies", replies);
   };
@@ -28,18 +31,25 @@
   onMount(fetchReplies);
 </script>
 
-<div id="circle-orbit-container" class:disabled>
-  {#each replies as reply, index (reply.textHash + reply.timestamp)}
-  <div
-  class="inner-orbit"
+<div
+  id="circle-orbit-container"
+  class:disabled
   style="
-    top: {62.5 + index * 10}px;
-    left: {62.5 + index * 10}px;
-    width: {125 - index * 20}px;
-    height: {125 - index * 20}px;
-    animation-duration: {5 + index}s;
+    height: {orbitDiameter}px;
+    width: {orbitDiameter}px;
   "
 >
+  {#each replies as reply, index (reply.textHash + reply.timestamp)}
+    <div
+      class="inner-orbit"
+      style="
+    top: {orbitDiameter / 4 + index * 10}px;
+    left: {orbitDiameter / 4 + index * 10}px;
+    width: {orbitDiameter / 2 - index * 20}px;
+    height: {orbitDiameter / 2 - index * 20}px;
+    animation-duration: {5 + index}s;
+  "
+    >
       <div class="inner-orbit-circles" />
     </div>
   {/each}
@@ -52,8 +62,6 @@
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
-    height: 250px;
-    width: 250px;
     z-index: 1;
   }
 
@@ -64,7 +72,7 @@
   /* ---------- Inner orbit - This is the circles closest to the central point ---------- */
   .inner-orbit {
     position: absolute;
-    border: 1px #4a437f dashed;
+    border: 1px #4a437f7e dashed;
     border-radius: 100%;
     -webkit-animation: spin-right linear infinite;
     animation: spin-right linear infinite;
@@ -72,9 +80,10 @@
 
   /* ---------- Repeating styles for the inner orbiting circles ---------- */
   .inner-orbit-circles {
+    top: 0; 
+    left: 50%; 
+    transform: translate(-50%, -50%);
     position: absolute;
-    top: 62px;
-    left: -6px;
     height: 10px;
     width: 10px;
     border-radius: 100%;
