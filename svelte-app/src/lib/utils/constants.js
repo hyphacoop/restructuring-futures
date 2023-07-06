b5fd9e53e1857cca1a0d448d7872e0415a13d2b4
vincent <vincentcharlebois@gmail.com>
2023-07-06 16:32:18 -0400

    add lunar and color cycles

---
+++ svelte-app/src/lib/utils/constants.js
@@ -0,0 +1,5 @@
+// durations in microseconds
+export const LUNAR_PHASE = [1918080000000, 1278720000000, 639360000000, 0];
+
+// color for each lunar phase
+export const COLOR_CYCLE = ["#fff5d9", "#d3e3d9", "#F9DFDD", "#CCE9F0"];
