2f4262021dcd05e77ac93c6f51ca474be3c0a54f
vincent <vincentcharlebois@gmail.com>
2023-07-31 13:46:11 -0400

    Create reusable tooltip component

---
+++ svelte-app/src/lib/Components/Tooltip.svelte
@@ -0,0 +1,23 @@
+<script>
+    import { onMount } from 'svelte';
+    let tooltipDiv;
+    export let text = '';
+    let showTooltip = false;
+    
+    let position = { top: 0, left: 0 };
+    
+    onMount(() => {
+      tooltipDiv.addEventListener('mouseover', () => { showTooltip = true; });
+      tooltipDiv.addEventListener('mouseout', () => { showTooltip = false; });
+    });
+  </script>
+  
+  <div bind:this={tooltipDiv} style="position: relative;">
+    <slot></slot>
+    {#if showTooltip}
+      <div style="position: absolute; top: 10px; left: 10px; background: lightgray; padding: 10px; border-radius: 5px; z-index: 1;">
+        {text}
+      </div>
+    {/if}
+  </div>
+  
