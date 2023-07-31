44c5707603c2a18a45c4df6f6925d3b2c85f8240
vincent <vincentcharlebois@gmail.com>
2023-07-31 13:45:51 -0400

    Create Studio Portal

---
+++ svelte-app/src/lib/Components/StudioPortal.svelte
@@ -0,0 +1,72 @@
+<script>
+    import { get } from 'svelte/store';
+    import settings from "../../store/settings";
+    import { studioShares } from "../../store/settings";
+    import shareKeypair from "../../store/share";
+    import Tooltip from "./Tooltip.svelte"; 
+
+    import { createEventDispatcher } from 'svelte';
+    
+    const dispatch = createEventDispatcher();
+
+    let selectedShare = get(shareKeypair).shareAddress;
+    
+    shareKeypair.subscribe(value => {
+      selectedShare = value.shareAddress;
+    });
+    
+    function updateShareKeypair() {
+      const shareAddress = selectedShare;
+      const secret = settings.shareSecrets[shareAddress]; // retrieve the secret
+      shareKeypair.set({shareAddress, secret}); // set both shareAddress and secret
+      // dispatch a 'shareUpdated' event
+      dispatch('shareUpdated');
+    }
+    
+    let shareList = [];
+    
+    studioShares.subscribe(value => {
+      shareList = [...value]; 
+    });
+    </script>
+    <div class='flex flex-col items-center justify-center'>
+        <div class='flex flex-col items-center justify-center max-w-3xl mx-auto w-full md:w-1/2'>
+            <Tooltip text="You can choose from any studio you saved in the share in settings. Settings can be accessed from the top left corner under 'workspace settings'.">
+                <p>You have reached the Beginning of Studio portals. <br>Choose the Studio you want to enter:</p>
+            </Tooltip>
+                <label for="share-switcher" class="text-center">Select Workspace</label>
+                <select
+                    id="share-switcher"
+                    bind:value="{selectedShare}"
+                    class="form-control"
+                >
+                    {#each shareList as share (share)}
+                        <option value={share}>{share}</option>
+                    {/each}
+                </select>
+                <button class='phase1 my-2' on:click="{updateShareKeypair}">enter selected studio</button>
+        </div>
+    </div>
+    <style>
+        p {
+            font-size: 1.2rem;
+            margin-bottom: 1rem;
+        }
+        label {
+            display: block;
+        }
+        div {
+            text-align: left;
+            padding: 1rem;
+            margin: 0.25rem;
+        }
+        select {
+            width: 90%;
+            font-size: 1rem;
+            margin: 0.25rem;
+            padding: 0.5rem 0.75rem;
+            border-radius: 0.25rem;
+            border: 1px solid #ccc;
+        }
+    </style>
+    
