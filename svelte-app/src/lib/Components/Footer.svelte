f1d5a493d35b80b5ad98d27fcfb666552e763a67
vincent <vincentcharlebois@gmail.com>
2023-08-01 14:20:40 -0400

    created basic footer

---
+++ svelte-app/src/lib/Components/Footer.svelte
@@ -0,0 +1,33 @@
+<footer class='w-full'>
+  <div class="footer m-3 flex flex-row justify-between">
+    <div class='flex flex-row justify-between w-1/3 ml-8'> 
+      <div>
+        read manual
+      </div>
+      <div>
+        report bugs
+      </div>
+      <div>
+        about
+      </div>
+    </div>
+    <div class='mr-8'>
+      open source project [link to repo???CTA for engagement??] thank funder?
+    </div>
+  </div>
+</footer>
+
+<style>
+    .footer {
+      position: fixed;
+      bottom: 0.25rem;
+      left: 0;
+      right: 0;
+      transition: transform 1.3s ease-in-out;
+      z-index: 999;
+      padding: 0.75rem;
+      border: 1px solid black;
+      background-color:white;
+    }
+  </style>
+  
