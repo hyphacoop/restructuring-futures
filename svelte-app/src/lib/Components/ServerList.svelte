8b65f814cc0c9cbe379e940b7c31029385da48d8
vincent <vincentcharlebois@gmail.com>
2023-08-02 09:20:24 -0400

    update server settings

---
+++ svelte-app/src/lib/Components/ServerList.svelte
@@ -0,0 +1,19 @@
+<script>
+  import settings from "../../store/settings";
+</script>
+
+{#if settings.servers.length > 0}
+  <div>
+   
+    <ul class='ml-0'>
+      {#each settings.servers as server, i (i)}
+        <li>{server}</li>
+      {/each}
+    </ul>
+  </div>
+{/if}
+<style>
+  ul {
+    list-style-type: inherit;
+  }
+</style>
---
+++ svelte-app/src/lib/Customization/Server.svelte
@@ -0,0 +1,24 @@
+<script>
+  import ServerList from "../Components/ServerList.svelte";
+  import AddServer from "../Server/AddServer.svelte";
+  import RemoveServer from "../Server/RemoveServer.svelte";
+
+  import settings from "../../store/settings";
+</script>
+<div class='flex flex-col items-start pb-24'>
+  <h3>
+      Server settings
+  </h3>
+  <h4>
+    Currrent {settings.servers.length === 1
+      ? "Server:"
+      : `${settings.servers.length} Servers:`}
+  </h4>
+  <div class='my-4'>
+    <ServerList />
+
+  </div>
+<AddServer />
+<RemoveServer />
+
+</div>
