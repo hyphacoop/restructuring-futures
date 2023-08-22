033a4f2af25a7d20add25d983cfb3356c8f2bd6a
vincent <vincentcharlebois@gmail.com>
2023-08-22 10:59:28 -0400

    Refactor pathToXY to handle multiple path structures
    - Added checks to determine the path structure based on its length.
    - Adjusted the extraction of X and Y coordinates based on the detected path structure.

--- svelte-app/src/lib/utils/pathToXY.js
+++ svelte-app/src/lib/utils/pathToXY.js
@@ -2,8 +2,21 @@ import numberToLetter from "./numberToLetter.js";
 
 export default function pathToXY(path) {
     let splitPath = path.split("/");
-    let selectedX = splitPath[2];
-    let selectedY = splitPath[3];
+
+    let selectedX, selectedY;
+    console.log('splitPath', splitPath)
+    console.log('splitPath length', splitPath.length)
+    // If there's no additional page subfolder
+    if (splitPath.length === 6) {
+        selectedX = splitPath[2];
+        selectedY = splitPath[3];
+    } 
+    // If there's an additional page subfolder
+    else if (splitPath.length === 7) {
+        selectedX = splitPath[3];
+        selectedY = splitPath[4];
+    } 
+
     let scaledX = Number(selectedX) + 1;
     let scaledY = numberToLetter(Number(selectedY));
     return [scaledX, scaledY];
