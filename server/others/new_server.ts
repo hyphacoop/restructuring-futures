dfbcf89abde1feb2792d00f3229877356b1176a1
Vincent <vincent@Helenas-MacBook-Air.local>
2023-08-23 18:49:21 -0400

    Improved server script

---
+++ server/others/new_server.ts
@@ -0,0 +1,35 @@
+import * as Earthstar from "https://deno.land/x/earthstar@v10.2.1/mod.ts";
+import { DocDriverSqliteFfi } from "https://deno.land/x/earthstar@v10.2.1/src/replica/doc_drivers/sqlite_ffi.ts";
+
+// Start the server.
+console.log("Starting up server...");
+new Earthstar.Server([
+  // Populate with shares from the a known shares list.
+  new Earthstar.ExtensionKnownShares({
+    knownSharesPath: "known_shares.json",
+    onCreateReplica: (shareAddress) => {
+      return new Earthstar.Replica(
+        {
+          driver: {
+            docDriver: new DocDriverSqliteFfi({
+              share: shareAddress,
+              filename: `./data/${shareAddress}.sql`,
+              mode: "create-or-open",
+            }),
+            attachmentDriver: new Earthstar.AttachmentDriverFilesystem(
+              `./data/${shareAddress}_attachments`,
+            ),
+          },
+        },
+      );
+    },
+  }),
+  // Add websocket syncing at /earthstar-api/v2
+  new Earthstar.ExtensionSyncWeb({
+    path: "/sync",
+  }),
+], { port: 8080 });
+
+
+// Log a helpful message about where to sync.
+console.log(`Sync with this server at IP ADDRESS/sync`);
