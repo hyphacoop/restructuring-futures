b58e335ba1488be188e4ecedf9722e778f9a2372
vincent <vincentcharlebois@gmail.com>
2023-08-24 13:48:00 -0400

    Add Electron App

---
+++ my-electron-app/main.js
@@ -0,0 +1,24 @@
+const { app, BrowserWindow } = require('electron');
+
+const createWindow = () => {
+let win = new BrowserWindow({
+  width: 800,
+  height: 600,
+  webPreferences: {
+    webSecurity: false,
+    nodeIntegration: true,
+    contextIsolation: false
+  }})
+
+  win.loadFile('index.html')
+}
+app.whenReady().then(() => {
+    createWindow()
+  
+    app.on('activate', () => {
+      if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+  })
+  app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+  })
