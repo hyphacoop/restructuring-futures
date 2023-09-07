9ace68da0492c324fe465a89ce12821e1eb4eec3
vincent <vincentcharlebois@gmail.com>
2023-09-07 13:22:37 -0400

    Enhance `extractPageNumber` function to handle document input
    - Add checks for undefined or null documents
    - Validate the presence and type of `doc.text` before extraction
    - Update the function parameter from `path`

--- svelte-app/src/lib/utils/extractPageNumber.js
+++ svelte-app/src/lib/utils/extractPageNumber.js
@@ -1,4 +1,10 @@
-export function extractPageNumber(path) {
-    const match = path.match(/\/documents\/page(\d+)/);
-    return match ? parseInt(match[1], 10) : 1; // default to 1 if no match
-}
+export function extractPageNumber(doc) {
+    if (!doc) {
+        console.error("Undefined document passed to extractPageNumber");
+        return;
+    }
+    if (doc.text && typeof doc.text === 'string' && doc.text.trim() !== "") {
+        const match = doc.path.match(/\/documents\/page(\d+)/);
+        return match ? parseInt(match[1], 10) : 1; // default to 1 if no match
+    }
+  }
