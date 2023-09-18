72cafabc52d532810ca366f886a5bfcce56912fd
vincent <vincentcharlebois@gmail.com>
2023-09-18 10:54:36 -0400

    Add List of path for all Icons in separate file

---
+++ svelte-app/src/lib/utils/icons.js
@@ -0,0 +1,59 @@
+export const flowers = [
+    "icons/flower-phase-0-02.png",
+    "icons/flower-phase-1-02.png",
+    "icons/flower-phase-2-02.png",
+    "icons/flower-phase-3-02.png",
+    "icons/flower-phase-4-02.png",
+    "icons/flower-phase-5-02.png",
+    "icons/flower-phase-6-02.png",
+    "icons/flower-phase-7-02.png",
+    "icons/flower-phase-8-02.png",
+    "icons/flower-phase-9-02.png",
+];
+
+export const mountains = [
+    "icons/mountain-phase-0-02.png",
+    "icons/mountain-phase-1-02.png",
+    "icons/mountain-phase-2-02.png",
+    "icons/mountain-phase-3-02.png",
+    "icons/mountain-phase-4-02.png",
+    "icons/mountain-phase-5-02.png",
+    "icons/mountain-phase-6-02.png",
+    "icons/mountain-phase-7-02.png",
+    "icons/mountain-phase-8-02.png",
+  ];
+
+export const planets = [
+    "icons/phaseplanet-1.png",
+    "icons/phaseplanet-2.png",
+    "icons/phaseplanet-3.png",
+    "icons/phaseplanet-4.png",
+    "icons/phaseplanet-5.png",
+    "icons/phaseplanet-6.png",
+    "icons/phaseplanet-7.png",
+    "icons/phaseplanet-8.png",
+];
+
+export const rivers = [
+    "icons/phaseriver-1.png",
+    "icons/phaseriver-2.png",
+    "icons/phaseriver-3.png",
+    "icons/phaseriver-4.png",
+    "icons/phaseriver-5.png",
+    "icons/phaseriver-6.png",
+    "icons/phaseriver-7.png",
+    "icons/phaseriver-8.png",
+]
+
+export const wishbones = [
+    "icons/phasewishbone-8.png",
+    "icons/phasewishbone-1.png",
+    "icons/phasewishbone-2.png",
+    "icons/phasewishbone-3.png",
+    "icons/phasewishbone-4.png",
+    "icons/phasewishbone-5.png",
+    "icons/phasewishbone-6.png",
+    "icons/phasewishbone-7.png",
+]
+
+
