d495f8824e1cd2868691e737316a623fa53ca880
vincent <vincentcharlebois@gmail.com>
2023-09-19 09:52:05 -0400

    Reorganized ShareViewer with an option to toggle visibility of shared items for a cleaner workspace.

--- svelte-app/src/lib/Components/ShareViewer.svelte
+++ svelte-app/src/lib/Components/ShareViewer.svelte
@@ -1,4 +1,6 @@
 <script>
+    import { slide } from 'svelte/transition';
+
     import { shares, secrets } from "../../store/settings";
     import settings from "../../store/settings";
     import { statusStore } from "../../store/status";
@@ -10,6 +12,8 @@
     let shareList = [];
     let tooltipStates = {};
     let tooltipTimers = {};
+
+    let showItems = true;
 
     function showShareTooltip(share, type) {
          // Clear any existing timers for this share
@@ -33,73 +37,93 @@
     $: console.log('shareSecrets', settings.shareSecrets);
 
 </script>
+
+<div class='flex flex-row items-start mb-4'>
+    <button 
+      class='{showItems ? "phase1" : "phase2"}' 
+      on:click={() => showItems = !showItems}
+      >
+      {#if showItems}
+        hide list
+      {:else}
+        show list
+      {/if}
+    </button>
+</div>
+
 <div class='flex flex-col items-start'>
-{#if shareList.length > 0}
-{#each shareList as share (share)}
-    <div class="flex flex-col items-start py-2">
-        <div class="flex flex-row justify-between">
-            <div class="flex flex-col items-start py-2">
+    {#if showItems}
+    <div transition:slide|local>
+    {#if shareList.length > 0}
+    {#each shareList as share (share)}
+        <div class="flex flex-col items-start py-2">
+            <div class="flex flex-row justify-between">
+                <div class="flex flex-col items-start py-2">
 
-        <p>
-            <b>Address:</b>
-            {share}
-        </p>
-        {#if $secrets[share]}
             <p>
-                <b>Secret:</b>
-                {$secrets[share]}
+                <b>Address:</b>
+                {share}
             </p>
-        {/if}
-        <p>
-            <b>Type:</b>
-            {share.includes('commons') ? 'Commons' : 'Studio'}
-        </p>
-        </div>
-<!--         {#if $statusStore[share]}
-            {#if $statusStore[share].docs}
-                {#if Object.keys($statusStore[share].docs).length > 0}        
-                <div class="ml-16">
-                    <StatusPanel shareID={share} />
-                </div>
-                {/if}
+            {#if $secrets[share]}
+                <p>
+                    <b>Secret:</b>
+                    {$secrets[share]}
+                </p>
             {/if}
-        {/if} -->
+            <p>
+                <b>Type:</b>
+                {share.includes('commons') ? 'Commons' : 'Studio'}
+            </p>
+            </div>
+            {#key share}
+                {#if $statusStore[share]}
+                    {#if $statusStore[share].docs}
+                        {#if Object.keys($statusStore[share].docs).length > 0}        
+                        <div class="ml-16">
+                            <StatusPanel shareID={share} />
+                        </div>
+                        {/if}
+                    {/if}
+                {/if} 
+            {/key}
+        </div>
+            <div class='flex flex-row items-end'>
+            <div class="mt-4 mx-2">
+                <CreateInvitation shareAddress={share} />
+            </div>
+            <div class="mt-4 mx-2">
+                <button class="phase1 tooltip" on:click={() => {
+                    navigator.clipboard.writeText(share);
+                    showShareTooltip(share, 'address');
+                }}>
+                    copy address
+                    <span class="tooltiptext" class:visible={tooltipStates[share] === 'address'}>address copied </span>
+
+                </button>
+            </div>
+            <div class="mt-4 mx-2">
+                <button class="phase1 tooltip" on:click={() => {
+                    navigator.clipboard.writeText(settings.shareSecrets[share]);
+                    showShareTooltip(share, 'secret');
+                }}>
+                    copy secret
+                    <span class="tooltiptext" class:visible={tooltipStates[share] === 'secret'}>secret copied</span>
+
+                </button>
+            </div>
+            <div class="mt-4 ml-16">
+
+                <DownloadTool shareAddress={share} />
+            </div>
+
+        </div>
+            <hr class="divider w-full mt-4 mb-2">
+        </div>
+
+    {/each}
+    {/if}
     </div>
-        <div class='flex flex-row items-end'>
-        <div class="mt-4 mx-2">
-            <CreateInvitation shareAddress={share} />
-        </div>
-        <div class="mt-4 mx-2">
-            <button class="phase1 tooltip" on:click={() => {
-                navigator.clipboard.writeText(share);
-                showShareTooltip(share, 'address');
-            }}>
-                copy address
-                <span class="tooltiptext" class:visible={tooltipStates[share] === 'address'}>address copied </span>
-
-            </button>
-        </div>
-        <div class="mt-4 mx-2">
-            <button class="phase1 tooltip" on:click={() => {
-                navigator.clipboard.writeText(settings.shareSecrets[share]);
-                showShareTooltip(share, 'secret');
-            }}>
-                copy secret
-                <span class="tooltiptext" class:visible={tooltipStates[share] === 'secret'}>secret copied</span>
-
-            </button>
-        </div>
-        <div class="mt-4 ml-16">
-
-            <DownloadTool shareAddress={share} />
-        </div>
-
-    </div>
-        <hr class="divider w-full mt-4 mb-2">
-    </div>
-
-{/each}
-{/if}
+    {/if}
 </div>
 <style>
 hr.divider {
