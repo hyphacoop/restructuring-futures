87084ae1d3c13f2b13a59cb24139282c3cb59f24
vincent <vincentcharlebois@gmail.com>
2023-09-19 12:38:24 -0400

    Improve comments on phase variables to facilitate Customization Instructions #130

--- svelte-app/src/lib/utils/constants.js
+++ svelte-app/src/lib/utils/constants.js
@@ -1,7 +1,8 @@
-// durations in microseconds
+// durations of each phase in microseconds
 export const LUNAR_PHASE = [1918080000000, 1278720000000, 639360000000, 0];
 
-// color for each lunar phase
+// color for each phase
 export const COLOR_CYCLE = ["#fff5d9", "#d3e3d9", "#F9DFDD", "#CCE9F0"];
 
+// name of each phase
 export const PHASE_NAME = ["spores", "emergence", "epic", "fizz"]
